0f3e2a693ab547df492dd1644a3a97cd920f7b31
Daniel Angelo <jsilvagoli36@gmail.com>
2022-11-17 11:11:01 -0300

    Add funciton calculer

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-function
+ function OpenAlert(){
+    alert("Botao Clicar");
+ }
+ function somar(v1, v2){
+    return parseFloat(v1) + parseFloat(v2);
+ }
+ function subtrair(v1, v2){
+    return parseFloat(v1) - parseFloat(v2);
+ }
+ function multiplicar(v1, v2){
+    return parseFloat(v1) * parseFloat(v2);
+ }
