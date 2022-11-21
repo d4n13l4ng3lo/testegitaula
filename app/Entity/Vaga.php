3137d3253eee57e5b0476b05e3c5d9c49420b074
Daniel Angelo <jsilvagoli36@gmail.com>
2022-11-21 15:05:37 -0300

    upando vagas um

---
+++ app/Entity/Vaga.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Entity;
+use \App\Db\Database;
+class Vaga{
+    public $id;
+    public $titulo;
+    public $descricao;
+    public $ativo;
+    public $data;
+    public function cadastrar(){
+        $this->data = date('y-m-d H:i:s');
+
+         $obDatabase = new Database('vagas');
+         $obDatabase->insert([
+            'titulo' => $this->titulo,
+            'descricao' => $this->descricao,
+            'ativo'=> $this->ativo,
+            'data' => $this->data
+         ]);
+
+
+    }
+    
+}
